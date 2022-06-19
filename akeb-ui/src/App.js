4d1c78a2d28d6e794c69d4d26f63a3817e50fa46
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-19 15:52:23 -0700

    web3 installed and connection to meta mask is completed

--- akeb-ui/src/App.js
+++ akeb-ui/src/App.js
@@ -1,9 +1,39 @@
+import React from 'react';
+import { useState } from "react"
+import Web3 from "web3"
+
 
 function App() {
+  const [web3, setWeb3] = useState()
+  const [address, setAddress] = useState()
+
+  console.log("Web3", web3)
+  console.log("address", address)
+
+
+  const onConnect = async () => {
+    if(typeof window !== "undefined" && typeof window.ethereum != "undefined") {
+      try {
+        await window.ethereum.request({method: "eth_requestAccounts"})
+        const web3 = new Web3(window.ethereum)
+        setWeb3(web3)
+        const addresses = await web3.eth.getAccounts()
+        setAddress(addresses[0])
+      } catch(error) {
+        console.log(error)
+      }
+    }
+    else {
+      console.error("Meta mask is not installed")
+    }
+  }
+
   return (
-    <div className="App">
+    <div>
       Auction
+      <button onClick={onConnect}>Connect</button>
     </div>
+    
   );
 }
 
