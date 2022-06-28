6e1a442965c7cf491380ada1018983f249c8daac
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-28 15:39:31 +0430

    onclick added to enter

--- akeb-ui/src/Steps/Enter.js
+++ akeb-ui/src/Steps/Enter.js
@@ -1,9 +1,16 @@
 import { Button } from 'antd'
-import React from 'react'
+import React, { useContext } from 'react'
+import StepStateContext from '../Context/StepStateContext'
 
 const Enter = () => {
+    const {stepsState, setStepsState} = useContext(StepStateContext)
+
+    const onEnterHandler = () => {
+        setStepsState(stepsState + 1)
+    }
+
     return (
-        <Button type="primary" size='large'>
+        <Button type="primary" size='large' onClick={onEnterHandler}>
             Enter the auction!
         </Button>
     )
