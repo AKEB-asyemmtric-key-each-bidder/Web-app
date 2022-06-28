662d51b7803ae2f661a069b80ee7f014a72a5a9f
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-28 07:31:30 +0430

    steps title updated

--- akeb-ui/src/Auction-Steps.js
+++ akeb-ui/src/Auction-Steps.js
@@ -12,9 +12,9 @@ const AuctionSteps = () => {
     return (
         <Steps className='Auction-Steps'>
             <Step status="finish" title="Connect" icon={<WalletFilled />} />
-            <Step status="finish" title="Enter Auction" icon={<RightCircleFilled />} />
+            <Step status="finish" title="Enter" icon={<RightCircleFilled />} />
             <Step status="process" title="Bid" icon={<MoneyCollectFilled />} />
-            <Step status="wait" title="Confirm winner" icon={<ExperimentFilled />} />
+            <Step status="wait" title="Confirm" icon={<ExperimentFilled />} />
             <Step status="wait" title="End" icon={<TrophyFilled />} />
         </Steps>
     )
