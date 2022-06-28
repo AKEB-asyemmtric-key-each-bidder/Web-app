1c11769586ff456669a6a161c8c77356d183cbc6
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-28 15:57:18 +0430

    form created for bid submission

--- akeb-ui/src/Steps/Bid.js
+++ akeb-ui/src/Steps/Bid.js
@@ -1,8 +1,29 @@
+import { Button, Form, InputNumber } from 'antd'
 import React from 'react'
 
 const Bid = () => {
     return (
-        <div>Bid</div>
+        <Form
+            layout='vertical'>
+            <Form.Item
+                label="Enter your bid"
+                rules={[
+                    {
+                        required:true,
+                        message:"Bid number is required"
+                    }
+                ]}>
+                <InputNumber
+                    style={{'minWidth':'200px'}}
+                    size="large"
+                    step="0.0000001"
+                    stringMode />
+            </Form.Item>
+            <Button type='primary' htmlType='submit' block size='large'>
+                Submit
+            </Button>
+            
+        </Form>
     )
 }
 
