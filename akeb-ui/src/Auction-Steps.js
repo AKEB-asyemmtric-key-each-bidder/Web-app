345e12a7cc15a471cc17c5220603b64cfb6935f6
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-28 07:27:24 +0430

    steps title changed and connect icon changed

--- akeb-ui/src/Auction-Steps.js
+++ akeb-ui/src/Auction-Steps.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { LoadingOutlined, SmileOutlined, SolutionOutlined, UserOutlined } from '@ant-design/icons';
+import { LoadingOutlined, SmileOutlined, SolutionOutlined, WalletFilled } from '@ant-design/icons';
 import { Steps } from "antd"
 import "./Styles/Styles.css"
 
@@ -11,10 +11,11 @@ const { Step } = Steps;
 const AuctionSteps = () => {
     return (
         <Steps className='Auction-Steps'>
-            <Step status="finish" title="Login" icon={<UserOutlined />} />
-            <Step status="finish" title="Verification" icon={<SolutionOutlined />} />
-            <Step status="process" title="Pay" icon={<LoadingOutlined />} />
-            <Step status="wait" title="Done" icon={<SmileOutlined />} />
+            <Step status="finish" title="Connect" icon={<WalletFilled />} />
+            <Step status="finish" title="Enter Auction" icon={<SolutionOutlined />} />
+            <Step status="process" title="Bid" icon={<LoadingOutlined />} />
+            <Step status="wait" title="Confirm winner" icon={<SmileOutlined />} />
+            <Step status="wait" title="End" icon={<SmileOutlined />} />
         </Steps>
     )
 }
