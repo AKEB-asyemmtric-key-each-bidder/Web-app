123a3512b0a4290b854f79dc6cf9ab14f77978a4
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-29 08:16:35 +0430

    blockchain vars added to context

--- akeb-ui/src/App.js
+++ akeb-ui/src/App.js
@@ -11,27 +11,34 @@ import StepStateContext from "./Context/StepStateContext";
 import Bid from "./Steps/Bid";
 import Confirm from "./Steps/Confirm";
 import End from "./Steps/End";
+import BlockchainContext from "./Context/BlockchainContext";
 
 function App() {
   // 0: Connect, 1: Enter
   // 2: Bid, 3: Confirm ,4: End
   const [stepsState, setStepsState] = useState(0);
 
+  const [web3, setWeb3] = useState();
+  const [address, setAddress] = useState();
+  const [contract, setContract] = useState();
+
   const steps = [<Connect />, <Enter />, <Bid />, <Confirm />, <End />];
 
-  console.log("step state", stepsState);
-
   return (
-    <StepStateContext.Provider value={{ stepsState, setStepsState }}>
-      <Row justify="center" gutter={[16, 24]}>
-        <Col span={20}>
-          <StepsBar />
-        </Col>
-      </Row>
-      <Row justify="center" align="middle" style={{ minHeight: "300px" }}>
-        {steps[stepsState]}
-      </Row>
-    </StepStateContext.Provider>
+    <BlockchainContext.Provider
+      value={{ web3, setWeb3, address, setAddress, contract, setContract }}
+    >
+      <StepStateContext.Provider value={{ stepsState, setStepsState }}>
+        <Row justify="center" gutter={[16, 24]}>
+          <Col span={20}>
+            <StepsBar />
+          </Col>
+        </Row>
+        <Row justify="center" align="middle" style={{ minHeight: "300px" }}>
+          {steps[stepsState]}
+        </Row>
+      </StepStateContext.Provider>
+    </BlockchainContext.Provider>
   );
 }
 
--- akeb-ui/src/Steps/Connect.js
+++ akeb-ui/src/Steps/Connect.js
@@ -1,9 +1,20 @@
 import { Button } from "antd";
 import React, { useContext } from "react";
+import BlockchainContext from "../Context/BlockchainContext";
 import StepStateContext from "../Context/StepStateContext";
 
 const Connect = () => {
   const { stepsState, setStepsState } = useContext(StepStateContext);
+  const {
+    web3,
+    setWeb3,
+    address,
+    setAddress,
+    contract,
+    setContract,
+  } = useContext(BlockchainContext);
+
+  console.log("Web3", web3);
 
   const onConnectHandler = () => {
     setStepsState(stepsState + 1);
