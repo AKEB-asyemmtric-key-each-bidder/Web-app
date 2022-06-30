bdb29e3898c8e71d01d68e554ffbd8cf0b34f56a
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-30 12:19:10 +0430

    passphrase UI created

--- akeb-ui/src/Steps/Passphrase.js
+++ akeb-ui/src/Steps/Passphrase.js
@@ -1,7 +1,33 @@
+import { Button, Form, Input } from "antd";
 import React from "react";
 
 const Passsphrase = () => {
-  return <div>Passsphrase</div>;
+  const onFinish = () => {
+    console.log("on finish clicked");
+  };
+
+  return (
+    <Form layout="vertical" name="passphraseForm" onFinish={onFinish}>
+      <Form.Item
+        label="Enter your passphrase"
+        name="passphrase"
+        rules={[
+          {
+            required: true,
+            message: "Passphrase is required",
+          },
+        ]}
+        help="Never share this key with anyone!"
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 };
 
 export default Passsphrase;
