33169f6b3cac1aca2f84cfd53e072c493f953aca
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-06-30 11:20:02 +0430

    bidder registered into smart contract

--- akeb-ui/src/Steps/Enter.js
+++ akeb-ui/src/Steps/Enter.js
@@ -1,24 +1,41 @@
 import { Button } from "antd";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import StepStateContext from "../Context/StepStateContext";
 import BlockchainContext from "../Context/BlockchainContext";
 
 const Enter = () => {
   const { stepsState, setStepsState } = useContext(StepStateContext);
   const { address, setAddress, contract } = useContext(BlockchainContext);
+  const [loading, setLoading] = useState(false);
 
   const onEnterHandler = () => {
-    getSampleString();
+    registerBidder();
+    setLoading(true);
     // setStepsState(stepsState + 1);
   };
 
-  const getSampleString = async () => {
-    const str = await contract.methods.getSampleString().call();
-    console.log("str", str);
+  const registerBidder = async () => {
+    await contract.methods
+      .registerBidder()
+      .send({ from: address }, function(error, res) {
+        setLoading(false);
+
+        if (error) {
+          console.log("error", error);
+          return;
+        }
+        console.log("res", res);
+      });
+    // console.log("str", str);
   };
 
   return (
-    <Button type="primary" size="large" onClick={onEnterHandler}>
+    <Button
+      type="primary"
+      size="large"
+      onClick={onEnterHandler}
+      loading={loading}
+    >
       Enter the auction!
     </Button>
   );
