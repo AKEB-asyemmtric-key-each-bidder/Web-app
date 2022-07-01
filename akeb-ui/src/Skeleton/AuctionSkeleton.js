896db28ce7a848d1dcf148b51c825a0349a3ee2a
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-07-01 09:41:48 +0430

    logo added

--- akeb-ui/src/Skeleton/AuctionSkeleton.js
+++ akeb-ui/src/Skeleton/AuctionSkeleton.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Layout, Menu, Space, Typography } from "antd";
+import { Image, Layout, Menu, Space, Typography } from "antd";
+import Logo from "../Statics/Images/Logo.png";
 
 import "../Styles/Styles.css";
 
@@ -9,8 +10,17 @@ const { Header, Content, Footer } = Layout;
 const AuctionSkeleton = ({ children }) => {
   return (
     <Layout>
-      <Header>
-        <Menu theme="dark" mode="horizontal" />
+      <Header style={{ background: "#F0F2F5" }}>
+        <div
+          style={{
+            float: "left",
+            width: "60px",
+            height: "100%",
+          }}
+        >
+          <Image src={Logo} width={50} height={50} />
+        </div>
+        <Menu mode="horizontal" />
         <Content>{children}</Content>
         <Footer className="footer">
           <Text>All rights reserved by University of Victoria</Text>
