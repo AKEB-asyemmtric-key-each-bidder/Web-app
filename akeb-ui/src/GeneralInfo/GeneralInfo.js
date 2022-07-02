a2fd3ccc844f7653b2e3a09e446f79e4fcaf3647
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-07-02 08:13:06 +0430

    values passed to general cards

--- akeb-ui/src/GeneralInfo/GeneralInfo.js
+++ akeb-ui/src/GeneralInfo/GeneralInfo.js
@@ -1,10 +1,12 @@
 import { Col, Row } from "antd";
 import React, { useContext } from "react";
+import BidsContext from "../Context/BidsContext";
 import BlockchainContext from "../Context/BlockchainContext";
 import GeneralInfoCard from "./GeneralInfoCard";
 
 const GeneralInfo = () => {
   const { address } = useContext(BlockchainContext);
+  const { nonce, bid, encodedBid } = useContext(BidsContext);
 
   return (
     <Row gutter={[8, 8]}>
@@ -12,13 +14,13 @@ const GeneralInfo = () => {
         <GeneralInfoCard title="Address" value={address} />
       </Col>
       <Col span={12}>
-        <GeneralInfoCard title="Nonce" value="nonce" />
+        <GeneralInfoCard title="Nonce" value={nonce} />
       </Col>
       <Col span={12}>
-        <GeneralInfoCard title="Bid" value="bid" />
+        <GeneralInfoCard title="Bid" value={bid} />
       </Col>
       <Col span={12}>
-        <GeneralInfoCard title="Encoded value" value="encoded" />
+        <GeneralInfoCard title="Encoded value" value={encodedBid} />
       </Col>
       <Col span={12}>
         <GeneralInfoCard title="Dispute" value="No" />
