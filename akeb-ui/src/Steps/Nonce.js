5832540072606c11629d7c1772da3d253f861715
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-07-02 09:08:11 +0430

    nonce section UI improved

--- akeb-ui/src/Steps/Nonce.js
+++ akeb-ui/src/Steps/Nonce.js
@@ -1,5 +1,6 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, Result } from "antd";
 import React, { useContext } from "react";
+import { RocketFilled } from "@ant-design/icons";
 import BidsContext from "../Context/BidsContext";
 import StepStateContext from "../Context/StepStateContext";
 
@@ -13,26 +14,37 @@ const Nonce = () => {
     setStepsState(stepsState + 1);
   };
 
+  const MyForm = () => {
+    return (
+      <Form layout="vertical" name="passphraseForm" onFinish={onFinish}>
+        <Form.Item
+          label="Enter your nonce"
+          name="nonce"
+          rules={[
+            {
+              required: true,
+              message: "Passphrase is required",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  };
+
   return (
-    <Form layout="vertical" name="passphraseForm" onFinish={onFinish}>
-      <Form.Item
-        label="Enter your nonce"
-        name="nonce"
-        rules={[
-          {
-            required: true,
-            message: "Passphrase is required",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+    <Result
+      icon={<RocketFilled />}
+      title="Please enter your nonce"
+      subTitle="Nonce will be added to bid for randomizing value and making it harder to guess"
+      extra={<MyForm />}
+    />
   );
 };
 
