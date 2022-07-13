2f4f3a153889cae8a37c4a4b585940f99b2a96a8
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-07-13 13:54:05 +0430

    gas costs added

--- akeb-ui/src/Modals/GasCostsModal.js
+++ akeb-ui/src/Modals/GasCostsModal.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Modal } from "antd";
+import { Divider, Modal, Tag, Typography } from "antd";
+
+const { Paragraph } = Typography;
 
 const GasCostModal = ({ visible, setVisible }) => {
   const cancelClicked = () => {
@@ -14,7 +16,36 @@ const GasCostModal = ({ visible, setVisible }) => {
       footer={null}
       onCancel={cancelClicked}
     >
-      Gas costs
+      <Paragraph>
+        These are estimated values and can slightly vary in different network
+        situations
+      </Paragraph>
+      <Divider />
+      <ul>
+        <li>
+          Register bidder : <Tag>0.00018224 Ether</Tag>
+        </li>
+        <Divider />
+        <li>
+          Submit bid : <Tag>0.00013614 Ether</Tag>
+        </li>
+        <Divider />
+        <li>
+          Submit winner information : <Tag>0.00013155 Ether</Tag>
+        </li>
+        <Divider />
+        <li>
+          Dispute : <Tag>0.00035623 Ether</Tag>
+        </li>
+        <Divider />
+        <li>
+          Get winner information : <Tag>0 Ether</Tag>
+        </li>
+        <Divider />
+        <li>
+          Get disputted information : <Tag>0 Ether</Tag>
+        </li>
+      </ul>
     </Modal>
   );
 };
