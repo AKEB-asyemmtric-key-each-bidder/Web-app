18377cf9348988c1d7b59f3313a238bff6f8a35b
Ehsan Ghasaei <32008442+Ehsan-Home@users.noreply.github.com>
2023-08-12 12:41:14 -0700

    Update GenerateNonce.js
    Nonce generation utilizes crypto built-in library to generate the nonce. The input size of nonce is 128 bits

--- akeb-ui/src/Logics/GenerateNonce.js
+++ akeb-ui/src/Logics/GenerateNonce.js
@@ -4,22 +4,36 @@
 //   return res[0];
 // };
 
-const generateNonce = () => {
-  let randomString = "";
-  const chars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (let i = 0; i < 30; i = i + 1) {
-    const randomNum = generateRandomNumber(chars);
-    randomString = randomString + chars.charAt(randomNum);
-  }
-  return randomString;
-};
+// const generateNonce = () => {
+//   let randomString = "";
+//   const chars =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+//   for (let i = 0; i < 30; i = i + 1) {
+//     const randomNum = generateRandomNumber(chars);
+//     randomString = randomString + chars.charAt(randomNum);
+//   }
+//   return randomString;
+// };
 
-const generateRandomNumber = (chars) => {
-  const randNum = Math.random() * 1000000000;
-  const randNumFloored = Math.floor(randNum);
-  const divideRemaining = randNumFloored % chars.length;
-  return divideRemaining;
-};
+// const generateRandomNumber = (chars) => {
+//   const randNum = Math.random() * 1000000000;
+//   const randNumFloored = Math.floor(randNum);
+//   const divideRemaining = randNumFloored % chars.length;
+//   return divideRemaining;
+// };
+
+// export default generateNonce;
+
+
+const { randomBytes } = require("crypto");
+
+// 32 hex valus = 32 * 4 = 128 bits
+const length = 32
+
+function generateNonce() {
+  const randomBytesGenerated = randomBytes(Math.ceil(length / 2));
+  return randomBytesGenerated.toString("ascii").slice(0, length);
+}
 
 export default generateNonce;
+
